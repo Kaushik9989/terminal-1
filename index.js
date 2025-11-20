edc41da0d4b6c66777b2a8187952df3a66f55266
Vivek Kaushik <vivekmanepalli08@gmail.com>
2025-11-20 22:29:35 +0530

    new

--- index.js
+++ index.js
@@ -64,12 +64,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log('[REQ]', req.method, req.originalUrl);
-  console.log('[HEADERS]', req.headers);
-  console.log('[BODY]', req.body);
-  next();
-});
+
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
